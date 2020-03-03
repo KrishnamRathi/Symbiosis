dc23f64405bd8611a0590981b80b3b4a8b6e0ddb
iharshit009 <krishnam.rathi21@gmail.com>
2020-03-03 10:04:01 +0530

    s

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
 from flask import Flask, render_template,request
-from flask_mysqldb import MySQL
 from flask_bootstrap import Bootstrap
 import yaml
 
 app = Flask(__name__)
-
-#Configure db
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST']=db['mysql_host']
-app.config['MYSQL_USER']=db['mysql_user']
-app.config['MYSQL_PASSWORD']=db['mysql_password']
-app.config['MYSQL_DB']=db['mysql_db']
-
-mysql=MySQL(app)
-
-@app.route('/farmers') 
-def users():
-    cur=mysql.connection.cursor()
-    resultValue= cur.execute("SELECT * FROM farmer")
-    return render_template('index.html')
 
 
 @app.route('/') 
@@ -34,11 +18,6 @@ def send():
     if request.method=='POST':
         Email=request.form['Email']
         Password=request.form['password']
-        cur=mysql.connection.cursor()
-        cur.execute("INSERT INTO farmers(email,password) VALUES(%s,%s)",(email,password))
-        mysql.connection.commit()
-        cur.close()
-        return 'success'
     return render_template('login.html')    
 
 app.run(host = "localhost", debug = True)
